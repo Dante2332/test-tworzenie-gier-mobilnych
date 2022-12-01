fc2da1dcf2b5bc518d06d78c3ba3aded94dd599a
Dante2332 <konrad.k.dabrowski@gmail.com>
2022-12-01 11:05:24 +0100

    Added Following player to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,14 @@ public class Enemy : MonoBehaviour
     public float speed = 4f;
     Transform target;
 
+    private void Update()
+    {
+       if (target != null)
+        {
+            float enemyMove = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, enemyMove);
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
